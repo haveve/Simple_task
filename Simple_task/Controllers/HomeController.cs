dea96bff1a992f970551e515a9947bb2dd20f206
IvanPogoidash <ipz224_pis@student.ztu.edu.ua>
2023-03-22 21:34:49 +0200

    Simple task has done

---
+++ Simple_task/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Simple_task.Models;
+using System.Diagnostics;
+
+namespace Simple_task.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(User user)
+        {  
+            if(ModelState.IsValid)
+               return View(user);
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Simple_task/Models/User.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Simple_task.Models
+{
+    public class User
+    {
+        [Display(Name = "Name")]
+        [Required(ErrorMessage = "Name does not exist")]
+        public string UserName { set; get; }
+    }
+}
